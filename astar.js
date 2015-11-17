46db9536e9ee332f8075c7ba26a425163a6c93e7
Wukai1220 <1183323265@qq.com>
2015-11-17 22:27:56 +0800

    astar first commit

---
+++ astar.js
@@ -0,0 +1,67 @@
+(function (definition) {
+    /* global module, define */
+
+})(function() {
+
+    function pathTo(node) {
+
+    }
+
+    function getHeap() {
+
+    }
+
+    function Graph(gridIn, options) {
+
+    }
+
+    Graph.prototype.init = function() {
+
+    };
+
+    Graph.prototype.cleanDirty = function() {
+
+    };
+
+    Graph.prototype.makeDirty = function() {
+
+    };
+
+    Graph.prototype.neighbors = function() {
+
+    };
+
+    Graph.prototype.toString = function() {
+
+    };
+
+
+    function GridNode(x, y, weight) {
+
+    }
+
+    GridNode.prototype.toString = function() {
+
+    };
+
+    GridNode.prototype.getCost = function(fromNeighbor) {
+
+    };
+
+    GridNode.prototype.isWall = function() {
+
+    };
+
+    function BinaryHeap(scoreFunction) {
+
+    }
+
+    BinaryHeap.prototype = {
+
+    };
+
+    return {};
+
+});
+
+
---
+++ demo.js
@@ -0,0 +1,40 @@
+var WALL = 0,
+    performance = window.performance;
+
+$(function() {
+
+});
+
+var css = {};
+
+function GraphSearch($graph, options, implementation) {
+
+}
+
+GraphSearch.prototype.setOption = function(opt) {
+
+};
+
+GraphSearch.prototype.initialize = function() {
+
+};
+
+GraphSearch.prototype.cellClicked = function($end) {
+
+};
+
+GraphSearch.prototype.drawDebugInfo = function() {
+
+};
+
+GraphSearch.prototype.nodeFromElement = function($cell) {
+
+};
+
+GraphSearch.prototype.animateNoPath = function() {
+
+};
+
+GraphSearch.prototype.animatePath = function(path) {
+
+};

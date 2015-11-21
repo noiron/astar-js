b72f6b3b838bd5fa315dce1696d538d124004e3f
Wukai1220 <1183323265@qq.com>
2015-11-22 01:13:28 +0800

    2015-11-12 commit

--- graph.js
+++ graph.js
@@ -14,14 +14,38 @@ function GraphSearch($graph, options, implementation) {
     this.search = implementation;
     this.opts = $.extend({wallFrequency:.1, debug:true, gridSize:10}, options);
     this.initialize();
-
 }
 
 GraphSearch.prototype.setOption = function(opt) {
-
+    this.opts = $.extend(this.opts, opt);
+    if (opt["debug"] || opt["debug"] == false) {
+        this.drawDebugInfo(opt["debug"]);
+    }
 };
 
 GraphSearch.prototype.initialize = function() {
+
+    var self = this;
+    var graphSet = [];
+    var $graph = this.$graph;
+    $graph.empty();
+
+    var cellWidth = ($graph.width() / this.opts.gridSize) - 2;  // -2 for border
+    var cellHeight = ($graph.height() / this.opts.gridSize) - 2;
+
+    log("height", cellHeight, $graph.height(), this.opts.gridSize);
+    log("width", cellWidth, $graph.width(), this.opts.gridSize);
+
+    var $cellTemplate = $("<span />").addClass("grid_item").width(cellWidth).height(cellHeight);
+    for (var x = 0; x < this.opts.gridSize; x++) {
+        var $row = $("<div class='clear' />");
+        $graph.append($row);
+
+        // TODO:
+        //
+
+    }
+
 
 };
 
@@ -44,3 +68,7 @@ GraphSearch.prototype.animateNoPath = function() {
 GraphSearch.prototype.animatePath = function(path) {
 
 };
+
+function GraphNode(x, y, $element) {
+
+}
